881477b9c2af39197cb8887c8eae7da14a8dd492
Filippo Rossi <rossifilippo2000@gmail.com>
2021-11-16 15:49:12 +0100

    es palindromo

--- es 1/js/script.js
+++ es 1/js/script.js
@@ -1,2 +1,21 @@
-// Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// Palidroma Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let yourWord = prompt("Inserisci parola");
+
+function check(word) {
+    let temp = word.split("").reverse().toString().replace(/,/g, "");
+    word = word.split("").toString().replace(/,/g, "");
+    if (temp == word){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (check(yourWord)){
+    console.log(yourWord + " è palindromo");
+} else {
+    console.log(yourWord + " non è palindromo");
+}
 
